da1823482461fb8f7a9a1e464b437f0e2ac57980
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-12-03 19:38:49 -0800

    Initial Commit

---
+++ MagicCardSearch/MagicCardSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicCardSearchApp.swift
+//  MagicCardSearch
+//
+//  Created by Sean Kelley on 2025-12-03.
+//
+
+import SwiftUI
+
+@main
+struct MagicCardSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MagicCardSearchUITests/MagicCardSearchUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MagicCardSearchUITestsLaunchTests.swift
+//  MagicCardSearchUITests
+//
+//  Created by Sean Kelley on 2025-12-03.
+//
+
+import XCTest
+
+final class MagicCardSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
