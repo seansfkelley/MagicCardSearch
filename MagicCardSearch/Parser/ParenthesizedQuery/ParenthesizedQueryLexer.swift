3b83779c93a1410118eeec9cbdaae195baf40ef8
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-12-19 20:28:38 -0800

    Rewrite the lexer.

--- MagicCardSearch/Parser/ParenthesizedQuery/ParenthesizedQueryLexer.swift
+++ MagicCardSearch/Parser/ParenthesizedQuery/ParenthesizedQueryLexer.swift
@@ -13,108 +13,74 @@ internal typealias LexedParenthesizedQueryToken = (
     String, ParenthesizedQueryParser.CitronTokenCode
 )
 
-internal func parseVerbatim(_ input: String) -> LexedParenthesizedQueryToken? {
-    return (input, .Verbatim)
+internal func parseOpenParen(_ input: String) -> LexedParenthesizedQueryToken? {
+    return (input, .OpenParen)
+}
+
+internal func parseCloseParen(_ input: String) -> LexedParenthesizedQueryToken? {
+    return (input, .CloseParen)
 }
 
 internal func parseOr(_ input: String) -> LexedParenthesizedQueryToken? {
     return (input, .Or)
 }
 
+internal func parseVerbatim(_ input: String) -> LexedParenthesizedQueryToken? {
+    return (input, .Verbatim)
+}
+
 internal func parseWhitespace(_ input: String) -> LexedParenthesizedQueryToken? {
-    return (input, .Whitespace)
+    return (input, .And)
 }
 
 func parseParenthesizedQuery(_ input: String) throws -> ParenthesizedQuery {
+    let trimmedInput = String(input.trimmingPrefix(/\s+/))
+    
     let lexer = CitronLexer<LexedParenthesizedQueryToken>(rules: [
-        .regexPattern(#"'[^']*('|$)"#, parseVerbatim), // highest priority
-        .regexPattern(#""[^"]*("|$)"#, parseVerbatim), // highest priority
-        .regexPattern(#"/[^/]*(/|$)"#, parseVerbatim), // highest priority
-        .regexPattern(#"(?! )or(?! )"#, parseOr),
-        .string("(", ("(", .OpenParen)),
-        .string(")", (")", .CloseParen)),
+        .regexPattern(#"\s*-?(\s*"#, parseOpenParen),
+        .regexPattern(#"\s*)\s*"#, parseOpenParen),
+        .regexPattern(#"\s+or\s+"#, parseOr),
+        // In principle we could have a single regex matching Verbatim things, but it would be
+        // horrible and wildly unreadable. Instead, we separate the balanced-characters parts into
+        // their own and define a non-whitespace-permitting rule for everything else, then
+        // post-process the tokens to coalesce adjacent ones.
+        .regexPattern(#"[^'"/ \n\t]+"#, parseVerbatim),
+        .regexPattern(#"'[^']*('|$)"#, parseVerbatim),
+        .regexPattern(#""[^"]*("|$)"#, parseVerbatim),
+        .regexPattern(#"/[^/]*(/|$)"#, parseVerbatim),
         .regexPattern(#"[ \n\t]+"#, parseWhitespace),
-        .regexPattern(#"."#, parseVerbatim),
     ])
     
-    // TODO:
-    //   1. trim whitespace from input and preserve it
-    //   2. lex into an intermediate type without ranges (just String!)
-    //   3. change lexing rules to include whitespace around parens and ORs
-    //   4. add a verbatim rule that starts with at least non-paren and continues until it hits
-    //      whitespace or a balanced character or a close paren
-    //   5. keep the whitespace rule, for when it's not next to an operator
-    //   6. post-process the lexed tokens to:
-    //      - add ranges
-    //      - coalesce adjacent Verbatims -- this is just a lexing convenience so I don't have to
-    //        write insane regexes, but I could also write insane regexes instead
-    //      - replace any whitespace with a synthetic AND token set to the whitespace's range
-    //        - this should be _all_ whitespace, since that that isn't assigned to a paren or an OR
-    //          is necessarily going to function as AND
-    //    7. run the tokens with the ranges through the parser
-    
-    // alternate idea: if whitespace not adjacent to operators is unambiguously an AND, why not:
-    //   - write those insane regexes to capture all filter-like things as single tokens
-    //   - make the parens and ORs take as much whitespace as possible
-    //   - treat all remaining whitespace as AND
-    //
-    // we'd still have to post-process the tokens to inject the ranges, but only that. and also trim
-    // whitespace at the start and preserve it, to avoid having to deal with whitespace that could
-    // potentially be neither an AND nor part of an operator.
-    
-    // also, uh oh, I forgot about negation
-    
-    // when this parser exists, we can find all filters, including those in progress, with the
-    // following algorithm:
-    //
-    // 1. try to parse the string, if yes, done
-    // 2. otherwise, count the number of open parenthesis (naively, excluding quoting and such)
-    // 3. append a dummy " nonfilter" to the search term
-    // 4. for each count of open parenthesis, append a close parenthesis, and then try to parse it
-    // 5. if it still doesn't ever parse, give up
-    //
-    // or just write a grammar that knows how to parse incomplete input. it's a simple enough language.
-    //
-    // this algorithm fails if the last term that's incomplete has unterminated quotes. how to detect
-    // those and try to close them? could permit parsing them as a literal if and only if they
-    // continue to the end of the input entirely.
-    
     var tokens: [(ParenthesizedQueryTokenContent, ParenthesizedQueryParser.CitronTokenCode)] = []
-    try lexer.tokenize(input) { token, code in
-        if code == .Verbatim, let previous = tokens.last, previous.1 == .Verbatim {
-            _ = tokens.popLast()
+    try lexer.tokenize(trimmedInput) { token, code in
+        if tokens.isEmpty {
+            tokens.append((
+                .init(content: token, range: trimmedInput.startIndex..<trimmedInput.index(trimmedInput.startIndex, offsetBy: token.count)),
+                code,
+            ))
+        } else if code == .Verbatim, let previous = tokens.last, previous.1 == .Verbatim {
+            tokens.removeLast()
+            let previousStart = previous.0.range.lowerBound
+            let previousEnd = previous.0.range.upperBound
             tokens.append((
                 .init(
                     content: "\(previous.0.content)\(token)",
-                    range: previous.0.range.lowerBound..<input.index(previous.0.range.upperBound, offsetBy: token.count),
+                    range: previousStart..<trimmedInput.index(previousEnd, offsetBy: token.count),
                 ),
                 .Verbatim,
             ))
-        } else if code == .Verbatim && tokens.count >= 2 && tokens[tokens.count - 2].1 == .Verbatim && tokens[tokens.count - 1].1 == .Whitespace {
-            
         } else {
+            let previousEnd = tokens.last!.0.range.upperBound
             tokens.append((
-                .init(content: token, range: input.startIndex..<token.endIndex),
+                .init(content: token, range: previousEnd..<trimmedInput.index(previousEnd, offsetBy: token.count)),
                 code,
             ))
         }
     }
     
     let parser = ParenthesizedQueryParser()
-    var lastToken: LexedParenthesizedQueryToken
-    try lexer.tokenize(input) { token, code in
-        if let last = lastToken, last.1 == .Verbatim && code == .Verbatim {
-            lastToken = (
-                .init(
-                    content: "\(last.0.content)\(token.content)",
-                    range: last.0.range.lowerBound..<token.range.upperBound,
-                ),
-                .Verbatim,
-            )
-        } else {
-            lastToken = token
-            try parser.consume(token: lastToken, code: code)
-        }
+    for (token, code) in tokens {
+        try parser.consume(token: token, code: code)
     }
     return try parser.endParsing()
 }
