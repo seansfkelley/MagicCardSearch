394356e543da5c78781c0b6cbcc0d340061fccd0
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-12-21 15:37:29 -0800

    Rewrite SearchFilter to be nicer.

--- MagicCardSearch/Parser/SearchFilter/SearchFilter.swift
+++ MagicCardSearch/Parser/SearchFilter/SearchFilter.swift
@@ -2,42 +2,33 @@ func doubleQuoteIfNecessary(_ string: String) -> String {
     return string.contains(" ") ? "\"\(string)\"" : string
 }
 
-enum SearchFilter: Equatable, Hashable, Codable {
-    case basic(SearchFilterContent)
-    case negated(SearchFilterContent)
+struct SearchFilter: Equatable, Hashable, Codable, CustomStringConvertible {
+    let negated: Bool
+    let content: SearchFilterContent
     
-    var queryStringWithEditingRange: (String, Range<String.Index>) {
-        switch self {
-        case .basic(let content):
-            return content.queryStringWithEditingRange
-        case .negated(let content):
-            let (string, range) = content.queryStringWithEditingRange
-            let negatedString = "-\(string)"
-            return (
-                negatedString,
-                negatedString
-                    .index(after: range.lowerBound)..<negatedString
-                    .index(after: range.upperBound)
-            )
-        }
+    var description: String {
+        "\(negated ? "-" : "")\(content)"
     }
     
-    var isKnownFilterType: Bool {
-        return switch self {
-        case .basic(let content): content.isKnownFilterType
-        case .negated(let content): content.isKnownFilterType
+    var suggestedEditingRange: Range<String.Index> {
+        let string = description
+        let contentRange = content.suggestedEditingRange
+        if negated {
+            return contentRange.offset(with: string, by: 1)
+        } else {
+            return contentRange
         }
     }
 }
 
 /// Quoting to preserve whitespace not required; any quotes present will be assumed to be part of the term to search for.
-enum SearchFilterContent: Equatable, Hashable, Codable {
+enum SearchFilterContent: Equatable, Hashable, Codable, CustomStringConvertible {
     case name(String, Bool)
     case regex(String, Comparison, String)
     case keyValue(String, Comparison, String)
-    case parenthesized(String)
+    case parenthesized(String) // TODO: Recursive case!
     
-    var queryStringWithEditingRange: (String, Range<String.Index>) {
+    var description: String {
         switch self {
         case .name(let name, let isExact):
             var prefix = ""
@@ -49,39 +40,44 @@ enum SearchFilterContent: Equatable, Hashable, Codable {
             if isExact {
                 prefix = "!\(prefix)"
             }
-            let prefixedName = "\(prefix)\(name)"
-            return (
-                "\(prefixedName)\(suffix)",
-                prefix.endIndex..<prefixedName.endIndex,
-            )
+            return "\(prefix)\(name)\(suffix)"
         case .regex(let key, let comparison, let regex):
-            let prefix = "\(key)\(comparison.symbol)"
-            let formatted = "\(prefix)\(regex)"
-            return (
-                formatted,
-                formatted.index(after: prefix.endIndex)..<formatted.index(before: formatted.endIndex)
-            )
+            return "\(key)\(comparison)\(regex)"
         case .keyValue(let key, let comparison, let value):
-            let prefix = "\(key)\(comparison.symbol)"
-            if value.contains(" ") {
-                let formatted = "\(prefix)\"\(value)\""
-                return (
-                    formatted,
-                    formatted.index(after: prefix.endIndex)..<formatted.index(before: formatted.endIndex)
-                )
+            return if value.contains(" ") {
+                "\(key)\(comparison)\"\(value)\""
             } else {
-                let formatted = "\(prefix)\(value)"
-                return (
-                    formatted,
-                    prefix.endIndex..<formatted.endIndex
-                )
+                "\(key)\(comparison)\(value)"
             }
         case .parenthesized(let content):
-            return (
-                content,
-                content.index(after: content.startIndex)..<content.index(before: content.endIndex)
-            )
+            return "(\(content))"
         }
+    }
+    
+    var suggestedEditingRange: Range<String.Index> {
+        let string = description
+        
+        let left: String.Index
+        let right: String.Index
+        
+        switch self {
+        case .name(let name, let isExact):
+            let needsQuotes = name.contains(" ")
+            left = string.index(string.startIndex, offsetBy: (isExact ? 1 : 0) + (needsQuotes ? 1 : 0))
+            right = string.index(string.endIndex, offsetBy: needsQuotes ? -1 : 0)
+        case .keyValue(let filter, let comparison, let value):
+            let needsQuotes = value.contains(" ")
+            left = string.index(string.startIndex, offsetBy: filter.count + comparison.description.count + (needsQuotes ? 1 : 0))
+            right = string.index(string.endIndex, offsetBy: needsQuotes ? -1 : 0)
+        case .regex(let filter, let comparison, _):
+            left = string.index(string.startIndex, offsetBy: filter.count + comparison.description.count + 1)
+            right = string.index(string.endIndex, offsetBy: -1)
+        case .parenthesized:
+            left = string.index(after: string.startIndex)
+            right = string.index(before: string.endIndex)
+        }
+        
+        return left..<right
     }
     
     var isKnownFilterType: Bool {
@@ -94,7 +90,7 @@ enum SearchFilterContent: Equatable, Hashable, Codable {
     }
 }
 
-enum Comparison: String, Codable, Hashable, Equatable {
+enum Comparison: String, Codable, Hashable, Equatable, CustomStringConvertible {
     case including = ":"
     case equal = "="
     case notEqual = "!="
@@ -103,7 +99,5 @@ enum Comparison: String, Codable, Hashable, Equatable {
     case greaterThan = ">"
     case greaterThanOrEqual = ">="
 
-    var symbol: String {
-        return self.rawValue
-    }
+    var description: String { rawValue }
 }
