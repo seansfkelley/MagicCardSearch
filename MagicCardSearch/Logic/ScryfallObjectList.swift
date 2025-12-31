26c9e05f39e91026f03eed2682fccbe68b9cc708
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-12-31 12:21:02 -0800

    Implement stateful search management objects.

---
+++ MagicCardSearch/Logic/ScryfallObjectList.swift
@@ -0,0 +1,87 @@
+//
+//  ScryfallObjectList.swift
+//  MagicCardSearch
+//
+//  Created by Sean Kelley on 2025-12-31.
+//
+import ScryfallKit
+import Logging
+
+private let logger = Logger(label: "ScryfallObjectList")
+
+class ScryfallObjectList<T: Codable & Sendable> {
+    public private(set) var value: LoadableResult<ObjectList<T>, SearchErrorState> = .unloaded
+
+    private var fetcher: (Int) async throws -> ObjectList<T>
+    private var nextPage = 1
+    private var task: Task<Void, Never>?
+
+    init(_ fetcher: @escaping (Int) async throws -> ObjectList<T>) {
+        self.fetcher = fetcher
+    }
+
+    public static func empty() -> ScryfallObjectList<T> {
+        ScryfallObjectList { _ in .empty() }
+    }
+
+    func loadNextPage() {
+        if case .loading = value {
+            logger.debug("declining to load next page: already loading")
+            return
+        }
+
+        if case .loaded(let list, _) = value, list.nextPage == nil {
+            logger.debug("declining to load next page: already at the end of the list")
+            return
+        }
+
+        logger.info("loading next page", metadata: [
+            "page": "\(nextPage)",
+        ])
+
+        task?.cancel()
+        value = .loading(value.latestValue, nil)
+
+        task = Task {
+            do {
+                let result = try await self.fetcher(self.nextPage)
+                guard !Task.isCancelled else { return }
+
+                logger.debug("successfully fetched next page", metadata: [
+                    "page": "\(nextPage)",
+                ])
+                nextPage += 1
+                value = .loaded(append(value.latestValue, result), nil)
+            } catch {
+                logger.error("error fetching next page", metadata: [
+                    "page": "\(nextPage)",
+                    "error": "\(error)",
+                ])
+                value = .errored(value.latestValue, SearchErrorState(from: error))
+            }
+        }
+    }
+
+    private func append(_ first: ObjectList<T>?, _ second: ObjectList<T>) -> ObjectList<T> {
+        guard let first else { return second }
+
+        return ObjectList(
+            data: first.data + second.data,
+            hasMore: second.hasMore,
+            nextPage: second.nextPage,
+            totalCards: first.totalCards,
+            warnings: first.warnings,
+        )
+    }
+
+
+    deinit {
+        task?.cancel()
+    }
+}
+
+extension ObjectList {
+    public static func empty() -> ObjectList<T> {
+        ObjectList<T>(data: [], hasMore: false, nextPage: nil, totalCards: 0, warnings: [])
+    }
+}
--- MagicCardSearch/Logic/ViewModels/SearchState.swift
+++ MagicCardSearch/Logic/ViewModels/SearchState.swift
@@ -4,18 +4,25 @@
 //
 //  Created by Sean Kelley on 2025-12-31.
 //
+import SwiftUI
 import Logging
+import ScryfallKit
 
 private let logger = Logger(label: "SearchState")
 
 // TODO: Remove this decorator once I have disentagled a bunch of the state management.
 @MainActor
+@Observable
 class SearchState {
+    public var searchText: String = ""
+    public var searchSelection: TextSelection?
+    public var filters: [SearchFilter] = []
+    public private(set) var results: ScryfallObjectList<Card>?
+
+    private let scryfall = ScryfallClient(networkLogLevel: .minimal)
     private let filterHistory: FilterHistoryStore
     private let searchHistory: SearchHistoryStore
     private let pinnedFilter: PinnedFilterStore
-
-    private(set) var searchError: Error?
 
     // TODO: This should eventually be private and instead we expose the suggestions themselves.
     public var suggestionProvider: CombinedSuggestionProvider {
@@ -36,5 +43,47 @@ class SearchState {
         self.filterHistory = filterHistory
         self.searchHistory = searchHistory
         self.pinnedFilter = pinnedFilter
+    }
+
+    public func clearAll() {
+        searchText = ""
+        searchSelection = nil
+        filters = []
+        results = nil
+    }
+
+    public func performSearch(withConfiguration config: SearchConfiguration) {
+        logger.info("starting new search", metadata: [
+            "filters": "\(filters)",
+            "configuration": "\(config)",
+        ])
+
+        guard !filters.isEmpty else {
+            logger.info("no search filters; skipping to empty result")
+            results = .empty()
+            return
+        }
+
+        do {
+            try searchHistory.recordSearch(with: filters)
+        } catch {
+            logger.error("error while recording search", metadata: [
+                "filters": "\(filters)",
+                "error": "\(error)",
+            ])
+        }
+
+        let query = filters.map { $0.description }.joined(separator: " ")
+        results = .init() { [weak self] page async throws in
+            guard let self else { return .empty() }
+
+            return try await scryfall.searchCards(
+                query: query,
+                unique: config.uniqueMode.toScryfallKitUniqueMode(),
+                order: config.sortField.toScryfallKitSortMode(),
+                sortDirection: config.sortOrder.toScryfallKitSortDirection(),
+                page: page,
+            )
+        }
     }
 }
