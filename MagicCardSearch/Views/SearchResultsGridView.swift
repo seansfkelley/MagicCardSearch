3dcd9c40daeb2fd80739968e89ad61d67806eeaf
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-01-05 12:59:24 -0800

    This view should never be shown in this case. Remove flavor texts.

--- MagicCardSearch/Views/SearchResultsGridView.swift
+++ MagicCardSearch/Views/SearchResultsGridView.swift
@@ -1,21 +1,6 @@
 import SwiftUI
 import ScryfallKit
 
-private let flavorTexts = [
-    "Infinite ideas flow through the Multiverse, waiting for an open mind.", // Brainstorm
-    "These seeds waited faithfully for the sun to rise again. Now, their patience is rewarded.", // Splendid Reclamation
-    "Opportunity isn't something you wait for. It's something you create.", // Opportunity
-    "Words of power never disappear. They sleep, awaiting those with the will to rouse them.", // Archaeomancer
-    "The Multiverse is filled with limitless power just waiting for someone to reach out and seize it.", // Pyretic Ritual
-    "Beneath crashing waves lies and ocean of secrets waiting to be explored.", // Thrasios, Triton Hero
-    "The worthy shall cultivate a nimble mind to perceive the glorious wonders that await them.", // Kefnet's Monument
-    "Untold riches await those who forsake the bustling world to search the secret, silent places.", // Shimmering Grotto
-]
-
-private func hourlyRotatingRandomFlavorText() -> String {
-    flavorTexts[Calendar.current.component(.hour, from: Date()) % flavorTexts.count]
-}
-
 struct SearchResultsGridView: View {
     let list: ScryfallObjectList<Card>
 
@@ -33,16 +18,7 @@ struct SearchResultsGridView: View {
         ZStack {
             // TODO: Clean this up.
             if case .unloaded = list.value {
-                VStack(alignment: .center) {
-                    Spacer()
-                    Text(hourlyRotatingRandomFlavorText())
-                        .font(.system(.body, design: .serif))
-                        .foregroundStyle(.secondary)
-                        .italic()
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                    Spacer()
-                }
+                EmptyView()
             } else if case .errored(let results, let error) = list.value, results?.data.isEmpty ?? true {
                 ContentUnavailableView {
                     Label(error.title, systemImage: error.iconName)
