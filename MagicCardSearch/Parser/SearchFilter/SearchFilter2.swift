b92d859ade15f2d0c1d839615feeda6167d09b7e
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-01-09 12:03:36 -0800

    Flattening.

--- MagicCardSearch/Parser/SearchFilter/SearchFilter2.swift
+++ MagicCardSearch/Parser/SearchFilter/SearchFilter2.swift
@@ -13,4 +13,35 @@ public enum SearchFilter2: Codable, Sendable, Hashable, Equatable, CustomStringC
     case term(Polarity, FilterTerm)
     case and(Polarity, [SearchFilter2])
     case or(Polarity, [SearchFilter2])
+
+    public func flattened() -> SearchFilter2 {
+        switch self {
+        case .term:
+            return self
+
+        case .and(let polarity, let filters):
+            var unwrappedFilters: [SearchFilter2] = []
+            for filter in filters.map({ $0.flattened() }) {
+                // Only unwrap contained ANDs that don't flip the polarity again.
+                if case .and(.positive, let subFilters) = filter {
+                    unwrappedFilters.append(contentsOf: subFilters)
+                } else {
+                    unwrappedFilters.append(filter)
+                }
+            }
+            return .and(polarity, unwrappedFilters)
+            
+        case .or(let polarity, let filters):
+            var unwrappedFilters: [SearchFilter2] = []
+            for filter in filters.map({ $0.flattened() }) {
+                // Only unwrap contained ORs that don't flip the polarity again.
+                if case .or(.positive, let subFilters) = filter {
+                    unwrappedFilters.append(contentsOf: subFilters)
+                } else {
+                    unwrappedFilters.append(filter)
+                }
+            }
+            return .or(polarity, unwrappedFilters)
+        }
+    }
 }
