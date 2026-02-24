22926f81523bc87b6a2fa5e35548aad4189b56f3
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-02-24 14:22:21 -0800

    Notes on colons.

--- MagicCardSearch/Logic/Autocomplete/ReverseEnumerationSuggestionProvider.swift
+++ MagicCardSearch/Logic/Autocomplete/ReverseEnumerationSuggestionProvider.swift
@@ -151,11 +151,15 @@ class ReverseEnumerationSuggestionProvider {
         if let setFilter = scryfallFilterByType["set"] {
             for set in sets {
                 valueToFilters[set.code.uppercased().replacing(/[^a-zA-Z0-9 ]/, with: ""), default: []].append(setFilter)
+                // n.b. Scryfall does NOT want any other characters like colons (e.g. "Avatar: the
+                // Last Airbender") as it will not match anything.
                 valueToFilters[set.name.replacing(/[^a-zA-Z0-9 ]/, with: ""), default: []].append(setFilter)
             }
         }
 
         if let blockFilter = scryfallFilterByType["block"] {
+            // n.b. Scryfall does NOT want any other characters like colons (e.g. "Avatar: the
+            // Last Airbender") as it will not match anything.
             for block in sets.compactMap({ $0.block?.replacing(/[^a-zA-Z0-9 ]/, with: "") }).uniqued() {
                 valueToFilters[block, default: []].append(blockFilter)
             }
