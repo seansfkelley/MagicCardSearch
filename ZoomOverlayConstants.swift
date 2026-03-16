50952138ec8382739b7690407a6663ab60de93e5
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-03-15 21:43:05 -0700

    Noodle the zoom constants.

--- ZoomOverlayConstants.swift
+++ ZoomOverlayConstants.swift
@@ -25,17 +25,17 @@ enum ZoomOverlayConstants {
 
     // MARK: - Pan
     /// Rubber-band resistance for panning past the edge. Smaller = more stretch.
-    static let panRubberBandCoefficient: CGFloat = 0.008
+    static let panRubberBandCoefficient: CGFloat = 0.01
     /// Rubber-band resistance for scale past min/max. Smaller = more stretch.
-    static let scaleRubberBandCoefficient: CGFloat = 0.15
+    static let scaleRubberBandCoefficient: CGFloat = 1.0
     /// Pan edge buffer as a fraction of the screen's smaller dimension.
     static let panEdgeBufferFraction: CGFloat = 0.1
 
     // MARK: - Fling
     /// Minimum pan velocity (pt/s) required to trigger a fling dismiss.
-    static let flingVelocityThreshold: CGFloat = 1700
+    static let flingVelocityThreshold: CGFloat = 2000
     /// How far the card travels in the fling direction before snapping back.
-    static let flingDistance: CGFloat = 250
+    static let flingDistance: CGFloat = 350
     /// How much scale is shed during the fling throw.
     static let flingScaleReduction: CGFloat = 0.15
 
