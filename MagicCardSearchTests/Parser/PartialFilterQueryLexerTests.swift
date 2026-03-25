9e4cc6074145f05dd75a63065e4528b603fc6714
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-03-25 15:28:31 -0700

    Add tests for recent changes to the lexer around apostrophes.

--- MagicCardSearchTests/Parser/PartialFilterQueryLexerTests.swift
+++ MagicCardSearchTests/Parser/PartialFilterQueryLexerTests.swift
@@ -4,16 +4,49 @@ import Testing
 // Serialized: Citron is not thread-safe.
 @Suite(.serialized)
 struct PartialFilterQueryLexerTests {
-    @Test<[(String, [(String, PartialFilterQueryParser.CitronTokenCode)])]>("with allowingUnuterminatedLiterals=true", arguments: [
+    @Test<[(String, [(String, PartialFilterQueryParser.CitronTokenCode)]?)]>("with allowingUnterminatedLiterals=false", arguments: [
         // Apostrophes are not misparsed as single-quotes
         (
             "urza's",
             [("urza's", .Verbatim)],
         ),
+        // Unterminated double quote
+        (
+            "\"lightning ",
+            nil,
+        ),
     ])
-    func allowingUnuterminatedLiteralsTrue(input: String, expected: [(String, PartialFilterQueryParser.CitronTokenCode)]) throws {
-        let result = try lexPartialFilterQuery(input)
-        let unwrapped = result.map { ($0.0.content, $0.1) }
-        #expect(unwrapped == expected)
+    func allowingUnterminatedLiteralsFalse(input: String, expected: [(String, PartialFilterQueryParser.CitronTokenCode)]?) throws {
+        let result = try? lexPartialFilterQuery(input, allowingUnterminatedLiterals: false)
+        if let expected {
+            let unwrapped = (try #require(result)).map { ($0.0.content, $0.1) }
+            // Explicitly specify the comparison function since the inferred version requires Equatable conformance.
+            #expect(unwrapped.elementsEqual(expected, by: ==))
+        } else {
+            #expect(result == nil)
+        }
+    }
+
+    @Test<[(String, [(String, PartialFilterQueryParser.CitronTokenCode)]?)]>("with allowingUnterminatedLiterals=true", arguments: [
+        // Apostrophes are not misparsed as single-quotes
+        (
+            "urza's",
+            [("urza's", .Verbatim)],
+        ),
+        // Unterminated double quote
+        (
+            "\"lightning ",
+            [("\"lightning ", .Verbatim)],
+        ),
+    ])
+    func allowingUnterminatedLiteralsTrue(input: String, expected: [(String, PartialFilterQueryParser.CitronTokenCode)]?) throws {
+        let result = try? lexPartialFilterQuery(input, allowingUnterminatedLiterals: true)
+        if let expected {
+            let unwrapped = (try #require(result)).map { ($0.0.content, $0.1) }
+            // Explicitly specify the comparison function since the inferred version requires Equatable conformance.
+            #expect(unwrapped.elementsEqual(expected, by: ==))
+        } else {
+            #expect(result == nil)
+        }
     }
 }
