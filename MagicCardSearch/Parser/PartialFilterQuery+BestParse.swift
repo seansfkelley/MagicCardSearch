6f57f06c1b5f0b64d8d9b63332793cdfab6e5223
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-03-27 09:56:59 -0700

    Break up the big method.

--- MagicCardSearch/Parser/PartialFilterQuery+BestParse.swift
+++ MagicCardSearch/Parser/PartialFilterQuery+BestParse.swift
@@ -19,7 +19,6 @@ public extension PartialFilterQuery {
         }
     }
 
-    // swiftlint:disable:next cyclomatic_complexity
     static func from(
         _ input: String,
         autoclosePairedDelimiters: Bool = false,
@@ -27,10 +26,10 @@ public extension PartialFilterQuery {
         let trimmed = input.trimmingCharacters(in: .whitespaces)
         guard !trimmed.isEmpty else { return .empty }
 
-        lazy var fallback: BestParse = if trimmed.hasPrefix("-") {
-            .fallback(.term(PolarityString(.negative, String(trimmed[trimmed.index(after: trimmed.startIndex)...]))))
+        lazy var fallback: PartialFilterQuery = if trimmed.hasPrefix("-") {
+            .term(PolarityString(.negative, String(trimmed[trimmed.index(after: trimmed.startIndex)...])))
         } else {
-            .fallback(.term(PolarityString(.positive, trimmed)))
+            .term(PolarityString(.positive, trimmed))
         }
 
         let tokens: [LexedPartialFilterQueryToken]
@@ -38,82 +37,92 @@ public extension PartialFilterQuery {
             tokens = try lexPartialFilterQuery(trimmed, allowingUnclosedLiterals: autoclosePairedDelimiters)
         } catch {
             logger.debug("failed to lex query error=\(error)")
-            return fallback
+            return .fallback(fallback)
         }
 
         guard !tokens.isEmpty else { return .empty }
 
-        let parser = PartialFilterQueryParser()
-
-        if autoclosePairedDelimiters {
-            let last = tokens.last!
-
-            let closingQuote: String?
-            if last.1 == .Verbatim {
-                closingQuote = switch PartialFilterTerm.from(last.0.content).content.term {
-                case .balanced, .bare: ""
-                case .unopened: nil
-                case .unclosed(let quote, _): quote.rawValue
-                }
-            } else {
-                closingQuote = ""
-            }
-
-            var unclosedParens = 0
-            for (_, kind) in tokens {
-                let increment = switch kind {
-                case .OpenParen: 1
-                case .CloseParen: -1
-                default: 0
-                }
-
-                unclosedParens += increment
-            }
-
-            if closingQuote == nil || unclosedParens < 0 {
-                return fallback
-            } else if !closingQuote!.isEmpty || unclosedParens > 0 {
-                do {
-                    for (token, code) in tokens[..<(tokens.count - 1)] {
-                        try parser.consume(token: token, code: code)
-                    }
-                    if let closingQuote, !closingQuote.isEmpty && tokens.last!.1 == .Verbatim {
-                        try parser.consume(
-                            // HACK: We know the parser doesn't care about the ranges, and the rest
-                            // of this method doesn't either, so stub it.
-                            token: .init(tokens.last!.0.content + closingQuote, tokens.last!.0.range),
-                            code: .Verbatim,
-                        )
-                    } else {
-                        try parser.consume(token: tokens.last!.0, code: tokens.last!.1)
-                    }
-                    for _ in 0..<unclosedParens {
-                        try parser.consume(
-                            // HACK: We know the parser doesn't care about the ranges, and the rest
-                            // of this method doesn't either, so stub it.
-                            token: .init(")", tokens.last!.0.range),
-                            code: .CloseParen,
-                        )
-                    }
-
-                    return .autoclosed(try parser.endParsing().flattened())
-                } catch {
-                    logger.debug("failed to parse query error=\(error)")
-                    return fallback
-                }
-            } else {
-                // fall through
-            }
+        if autoclosePairedDelimiters,
+           let autoclosedResult = tryParsingWithAutoclose(tokens: tokens, withFallback: fallback) {
+            return autoclosedResult
         }
 
         do {
+            let parser = PartialFilterQueryParser()
             for (token, code) in tokens {
                 try parser.consume(token: token, code: code)
             }
             return .valid(try parser.endParsing().flattened())
         } catch {
             logger.debug("failed to parse query error=\(error)")
-            return fallback
+            return .fallback(fallback)
+        }
+    }
+
+    private static func tryParsingWithAutoclose(
+        tokens: [LexedPartialFilterQueryToken],
+        withFallback fallback: @autoclosure () -> PartialFilterQuery,
+    ) -> BestParse? {
+        guard !tokens.isEmpty else { return nil }
+
+        let last = tokens.last!
+
+        let closingQuote: String? = if last.1 == .Verbatim {
+            switch PartialFilterTerm.from(last.0.content).content.term {
+            case .balanced, .bare: ""
+            case .unopened: nil
+            case .unclosed(let quote, _): quote.rawValue
+            }
+        } else {
+            ""
+        }
+
+        var unclosedParens = 0
+        for (_, kind) in tokens {
+            let increment = switch kind {
+            case .OpenParen: 1
+            case .CloseParen: -1
+            default: 0
+            }
+
+            unclosedParens += increment
+        }
+
+        if closingQuote == nil || unclosedParens < 0 {
+            return .fallback(fallback())
+        } else if !closingQuote!.isEmpty || unclosedParens > 0 {
+            do {
+                let parser = PartialFilterQueryParser()
+
+                for (token, code) in tokens[..<(tokens.count - 1)] {
+                    try parser.consume(token: token, code: code)
+                }
+                if let closingQuote, !closingQuote.isEmpty && tokens.last!.1 == .Verbatim {
+                    try parser.consume(
+                        // HACK: We know the parser doesn't care about the ranges, and the rest
+                        // of this method doesn't either, so stub it.
+                        token: .init(tokens.last!.0.content + closingQuote, tokens.last!.0.range),
+                        code: .Verbatim,
+                    )
+                } else {
+                    try parser.consume(token: tokens.last!.0, code: tokens.last!.1)
+                }
+                for _ in 0..<unclosedParens {
+                    try parser.consume(
+                        // HACK: We know the parser doesn't care about the ranges, and the rest
+                        // of this method doesn't either, so stub it.
+                        token: .init(")", tokens.last!.0.range),
+                        code: .CloseParen,
+                    )
+                }
+
+                return .autoclosed(try parser.endParsing().flattened())
+            } catch {
+                logger.debug("failed to parse query error=\(error)")
+                return .fallback(fallback())
+            }
+        } else {
+            return nil
         }
     }
 }
