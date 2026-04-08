bf9d594143be3437a22e363f400cb3160f269eee
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-04-08 12:52:03 -0700

    Update SearchError to introspect HTTP a bit more.

--- MagicCardSearch/Logic/Search/SearchError.swift
+++ MagicCardSearch/Logic/Search/SearchError.swift
@@ -2,57 +2,83 @@ import SwiftUI
 import ScryfallKit
 
 enum SearchError: Error {
-    case clientError // 4xx errors (excluding 404 from searches)
-    case serverError // 5xx errors
-    case networkError // Connection issues
-    
+    case rateLimited
+    case clientError
+    case serverError
+    case networkError
+    case unknownError
+
     init(from error: Error) {
-        // Check for ScryfallError first
-        if let scryfallKitError = error as? ScryfallKitError,
-           case .scryfallError(let scryfallError) = scryfallKitError {
-            let statusCode = scryfallError.status
-            if (400..<500).contains(statusCode) {
-                self = .clientError
-            } else if (500..<600).contains(statusCode) {
-                self = .serverError
-            } else {
-                self = .networkError
+        self = if let scryfallKitError = error as? ScryfallKitError {
+            switch scryfallKitError {
+            case .scryfallError(let scryfallError):
+                if (400..<500).contains(scryfallError.status) {
+                    .clientError
+                } else if (500..<600).contains(scryfallError.status) {
+                    .serverError
+                } else {
+                    .unknownError
+                }
+            case .httpError(let status, _):
+                if status == 429 {
+                    .rateLimited
+                } else if (400..<500).contains(status) {
+                    .clientError
+                } else if (500..<600).contains(status) {
+                    .serverError
+                } else {
+                    .unknownError
+                }
+            default:
+                .unknownError
             }
         } else {
-            self = .networkError
+            .networkError
         }
     }
     
     var title: String {
         switch self {
+        case .rateLimited:
+            "Rate Limited"
         case .clientError:
             "Search Error"
         case .serverError:
             "Scryfall is Unavailable"
         case .networkError:
             "Connection Error"
+        case .unknownError:
+            "Unknown Error"
         }
     }
     
     var description: String {
         switch self {
+        case .rateLimited:
+            "You are being rate limited by Scryfall. Slow your roll!"
         case .clientError:
             "There was a problem with your search. Please check your filters and try again."
         case .serverError:
             "Scryfall is experiencing issues. Please try again in a moment."
         case .networkError:
             "Unable to connect to Scryfall. Please check your internet connection and try again."
+        case .unknownError:
+            "An unknown error occured."
         }
     }
     
     var iconName: String {
         switch self {
+        case .rateLimited:
+            "tachometer"
         case .clientError:
             "exclamationmark.magnifyingglass"
         case .serverError:
             "server.rack"
         case .networkError:
-            "wifi.slash"
+            "wifi.exclamationmark"
+        case .unknownError:
+            "questionmark.circle.dashed"
         }
     }
 }
